cde3fc7a1311fe544f3f3b3111ead45dfd06ca63
sidgharge <siddharth.gharge@bridgelabz.com>
2018-06-13 12:41:04 +0530

    user-service registration and login done

---
+++ ms-userservice/src/main/java/com/fundoonote/msuserservice/models/User.java
@@ -0,0 +1,62 @@
+package com.fundoonote.msuserservice.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String name;
+
+	private String email;
+
+	private String contact;
+
+	private String password;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
