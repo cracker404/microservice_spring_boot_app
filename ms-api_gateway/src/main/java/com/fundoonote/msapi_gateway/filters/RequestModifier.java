be8ca6da618d7ef6fa1bdfe3b552e19cc2896dc8
sidgharge <siddharth.gharge@bridgelabz.com>
2018-06-26 16:22:12 +0530

    before merge

--- ms-api_gateway/src/main/java/com/fundoonote/msapi_gateway/filters/RequestModifier.java
+++ ms-api_gateway/src/main/java/com/fundoonote/msapi_gateway/filters/RequestModifier.java
@@ -1,6 +1,5 @@
 package com.fundoonote.msapi_gateway.filters;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
--- ms-noteservice/src/main/java/com/fundoonote/msnoteservice/utility/Test.java
+++ ms-noteservice/src/main/java/com/fundoonote/msnoteservice/utility/Test.java
@@ -1,7 +1,6 @@
 package com.fundoonote.msnoteservice.utility;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
